acdacfe056bfe6fc7c9eb62dff4e077af59b8170
nelegarreta <nelegarreta@gmail.com>
2023-03-21 19:18:32 -0700

    Added logo header

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -44,7 +44,7 @@ function App() {
 
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <a class="navbar-brand" href="/">
-                    <img src="/ClientApp/src/components/images/Logoheader.png" width="30" height="30" alt="" />
+                    <img src="/images/Logoheader.png" width="30" height="30" alt="" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
